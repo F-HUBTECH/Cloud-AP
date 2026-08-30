f1056d95e3fbdaf787cc06172dafa0d0e2d8a2e8
Nipornpun <nipornpun.nj@gmail.com>
2026-08-30 11:21:35 +0700

    feat: reduce padding in main content area of protected layout

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -11,7 +11,7 @@ export default function ProtectedLayout({
       <Sidebar />
       <div className="flex flex-1 flex-col overflow-hidden">
         <Header />
-        <main className="flex-1 overflow-y-auto bg-muted/30 p-6">
+        <main className="flex-1 overflow-y-auto bg-muted/30 p-4">
           {children}
         </main>
       </div>
